5873b14c36927796b8c4de4678bb92c95323465b
Michael Brown <Michael Brown>
2017-01-20 13:18:53 -0500

    Second Commit

--- Spites.js
+++ Spites.js
@@ -1,16 +1,22 @@
 function SpriteSheet(path, frameWidth, frameHeight) {
 
-var image = new Image();
-var framesPerRow;
+//code removed for brevity
 
-//calculate the number of frames in a row after the image loads
+var currentFrame = 0; //the current frame to draw
+var counter = 0;      //keep track of frame rate
 
-var self = this;
-image.onload = function() {
-  framesPerRow = Math.floor(image.width / frameWidth);
+//Update the animation
+this.update = function() {
+
+  //update to the next frame if it is time
+
+  if (counter == (frameSpeed - 1))
+    currentFrame = (currentFrame + 1) % endFrame;
+}
+
+//update the counter
+counter = (counter + 1) % frameSpeed;
+
+//calculate the number of frames in a row after the image load
 
 };
-
-image.src ="Rike-Running.png"
-
-}
